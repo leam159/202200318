55e5895178faeca0e320bb3b1cd6063b11630b2c
leam159 <61855478+leam159@users.noreply.github.com>
2020-03-18 15:47:11 +0900

    방데이터 추가=>roomdatas추가

--- app/src/main/java/kr/co/tjoeun/a202200318/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a202200318/MainActivity.java
@@ -35,5 +35,19 @@ public class MainActivity extends BaseActivity {
     public void setValues() {
     roomadepter = new Roomadepter(mContext,R.layout.room_list_item,roomdatas);
     binding.roomlistView.setAdapter(roomadepter);
+
+    addRooms();
     }
+
+    private void addRooms() {
+    roomdatas.add(new Room(8000,"서울시동대문구",4,"학생원룸"));
+        roomdatas.add(new Room(8000,"서울시남대문구",2,"좋은땅"));
+        roomdatas.add(new Room(8000,"서울시노원구",1,"싼방"));
+        roomdatas.add(new Room(600000,"경기도안천시",0,"무료방"));
+        roomdatas.add(new Room(90000,"경기도안천시",-1,"살기편한방"));
+
+    roomadepter.notifyDataSetChanged();
+    }
+
+
 }
---
+++ app/src/main/java/kr/co/tjoeun/a202200318/adepters/Roomadepter.java
@@ -0,0 +1,42 @@
+package kr.co.tjoeun.a202200318.adepters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import kr.co.tjoeun.a202200318.R;
+import kr.co.tjoeun.a202200318.datas.Room;
+
+public class Roomadepter extends ArrayAdapter<Room> {
+
+    Context mContext;
+    List<Room> mList;
+    LayoutInflater inf;
+
+    public Roomadepter(@NonNull Context context, int resource, @NonNull List<Room> objects) {
+        super(context, resource, objects);
+
+        mContext = context;
+        mList = objects;
+        inf = LayoutInflater.from(mContext);
+
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+        if (row==null){
+            row= inf.inflate(R.layout.room_list_item,null);
+        }
+        return row;
+    }
+}
